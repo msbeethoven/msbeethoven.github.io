88b2bee272bedddcdeeea78e8a0db522c27d4e10
core <seorisachs@gmail.com>
2019-01-05 22:26:21 -0500

    simple addEventListener test

---
+++ tv.js
@@ -0,0 +1,10 @@
+console.log('home page');
+
+const something = document.getElementsByClassName('tv')
+console.log(something)
+
+document.addEventListener('click', (e) => console.log('i got clicked!'));
+
+// something.addEventListner('click', (e) => {
+//   console.alert("um this does work")
+// })
