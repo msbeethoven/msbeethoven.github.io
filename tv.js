6e0b99f0ef0b4cbcd1292df26976ca6014c40e17
core <seorisachs@gmail.com>
2019-01-09 14:23:41 -0500

    modal progress

--- tv.js
+++ tv.js
@@ -1,10 +1,52 @@
-console.log('home page');
+//this closes another modal as another one is opened 
+function closeAllProjects(className){
+  let selectedElms = document.getElementsByClassName(`${className}`)
+  for (let i = 0; i < selectedElms.length; i++){
+      selectedElms[i].style.visibility = 'hidden'
+  }
+}
 
-const something = document.getElementsByClassName('tv')
-console.log(something)
+//this dynamically closes the modals, regardless of which one you press
+let x = document.getElementsByClassName('close')
+function closeOneProject (htmlCol){
+    for (let i=0; i<htmlCol.length; i++){
+      console.log('lenght', htmlCol.length)
+        document.addEventListener('click', (e) => {
+            let parentElmId = e.srcElement.offsetParent.id
+            document.getElementById(parentElmId).style.visibility = 'hidden';
+        })
+    }
+}
 
-document.addEventListener('click', (e) => console.log('i got clicked!'));
+closeOneProject(x);
 
-// something.addEventListner('click', (e) => {
-//   console.alert("um this does work")
-// })
+
+
+
+
+
+
+
+let clickBox = document.getElementById('lucentInfo')
+window.onclick = function(event) {
+  console.log("this", clickBox)
+  if (event.target === window) {
+    console.log('whats', clickBox)
+    clickBox.style.display = 'none';
+  }
+}
+
+let codingismagic = document.getElementById('codingismagic');
+codingismagic.addEventListener('click', () => {
+  closeAllProjects('clickBox');
+  let codingismagicInfo = document.getElementById('codingismagicInfo');
+  codingismagicInfo.style.visibility = 'visible';
+})
+
+let lucent = document.getElementById('lucent');
+lucent.addEventListener('click', () => {
+  closeAllProjects('clickBox');
+  let lucentInfo = document.getElementById('lucentInfo');
+  lucentInfo.style.visibility = 'visible';
+})
+
