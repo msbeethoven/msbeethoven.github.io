7e38337c81a7ea84627cc86aa010bbaa499afb1b
core <seorisachs@gmail.com>
2019-01-10 15:18:02 -0500

    basic modal functionality

--- tv.js
+++ tv.js
@@ -27,14 +27,14 @@ closeOneProject(x);
 
 
 
-let clickBox = document.getElementById('lucentInfo')
-window.onclick = function(event) {
-  console.log("this", clickBox)
-  if (event.target === window) {
-    console.log('whats', clickBox)
-    clickBox.style.display = 'none';
-  }
-}
+// let clickBox = document.getElementById('lucentInfo')
+// window.onclick = function(event) {
+//   console.log("this", clickBox)
+//   if (event.target === window) {
+//     console.log('whats', clickBox)
+//     clickBox.style.display = 'none';
+//   }
+// }
 
 let codingismagic = document.getElementById('codingismagic');
 codingismagic.addEventListener('click', () => {
@@ -48,5 +48,47 @@ lucent.addEventListener('click', () => {
   closeAllProjects('clickBox');
   let lucentInfo = document.getElementById('lucentInfo');
   lucentInfo.style.visibility = 'visible';
+})
+
+let rocks4shale = document.getElementById('rocks4shale');
+rocks4shale.addEventListener('click', () => {
+  closeAllProjects('clickBox');
+  let rocks4shaleInfo = document.getElementById('rocks4shaleInfo');
+  rocks4shaleInfo.style.visibility = 'visible';
+})
+
+let turningoffthescreen = document.getElementById('turningoffthescreen');
+turningoffthescreen.addEventListener('click', () => {
+  closeAllProjects('clickBox');
+  let turningOffInfo = document.getElementById('turningOffInfo');
+  turningOffInfo.style.visibility = 'visible';
+})
+
+let kulak = document.getElementById('kulak');
+kulak.addEventListener('click', () => {
+  closeAllProjects('clickBox');
+  let turningOffInfo = document.getElementById('kulakInfo');
+  turningOffInfo.style.visibility = 'visible';
+})
+
+let dontdelete = document.getElementById('dontdelete');
+dontdelete.addEventListener('click', () => {
+  closeAllProjects('clickBox');
+  let turningOffInfo = document.getElementById('dontdeleteInfo');
+  turningOffInfo.style.visibility = 'visible';
+})
+
+let thoughtsandprayers = document.getElementById('thoughtsandprayers');
+thoughtsandprayers.addEventListener('click', () => {
+  closeAllProjects('clickBox');
+  let thoughtsandprayersInfo = document.getElementById('thoughtsandprayersInfo');
+  thoughtsandprayersInfo.style.visibility = 'visible';
+})
+
+let newyorkonion = document.getElementById('newyorkonion');
+newyorkonion.addEventListener('click', () => {
+  closeAllProjects('clickBox');
+  let newyorkonionInfo = document.getElementById('newyorkonionInfo');
+  newyorkonionInfo.style.visibility = 'visible';
 })
 
