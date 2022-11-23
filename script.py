9a275e7f79009f3b7e7c0abd0f5e2befc1118914
core <msbeethoven@users.noreply.github.com>
2022-11-23 03:28:11 -0500

    trying flask site w/o template folder so index.html is on top level

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return render_template("home.html", title="seori")
+  return render_template("index.html", title="seori")
 
 @app.route('/about')
 def about():
